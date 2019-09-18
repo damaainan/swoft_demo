d9fd93e2159bdd6c73c99021185d9b397defac88
damaainan <damaainan@163.com>
2019-09-18 17:16:32 +0800

    swagger bootstrap ui

--- swaggerdoc.sh
+++ swaggerdoc.sh
@@ -4,3 +4,5 @@
 php vendor/zircote/swagger-php/bin/openapi app/Doc/ -o public/swagger/swagger.json
 # php vendor/zircote/swagger-php/bin/openapi app/Doc/ -o static/swagger-doc/swagger.json
 
+# 写一个 php 脚本处理 json 文件为符合要求的格式
+# php deal
